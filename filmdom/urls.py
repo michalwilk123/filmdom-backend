9a21b0e473d29e70831e2455ae6bd013a74217fe
michal <michal.wilk0@yahoo.com>
2021-08-05 13:30:43 +0200

    added admin routes

--- filmdom/urls.py
+++ filmdom/urls.py
@@ -4,6 +4,8 @@ from filmdom_mvp import views
 from filmdom import settings
 from django.conf.urls.static import static
 from rest_framework.authtoken.views import obtain_auth_token
+from django.contrib import admin
+from filmdom_mvp import models
 
 router = routers.DefaultRouter()
 router.register("users", views.UserViewSet)
@@ -13,8 +15,15 @@ router.register("directors", views.DirectorViewSet)
 router.register("actors", views.ActorViewSet)
 router.register("genres", views.MovieGenreViewSet)
 
+admin.site.register(models.Actor)
+admin.site.register(models.MovieGenre)
+admin.site.register(models.Movie)
+admin.site.register(models.Comment)
+admin.site.register(models.Director)
+
 urlpatterns = [
     path("", include(router.urls)),
+    path('admin/', admin.site.urls),
     path("api-token-auth/", obtain_auth_token),
     path("auth/", views.AuthTestView.as_view()),
 ]
